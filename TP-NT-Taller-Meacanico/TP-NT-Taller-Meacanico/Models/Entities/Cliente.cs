708958592813a7f2fe8504c76caef5a285eae009
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2018-11-01 20:56:17 -0300

    agrego un par de cositas a cliente

--- TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Models/Entities/Cliente.cs
+++ TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Models/Entities/Cliente.cs
@@ -9,5 +9,15 @@ namespace TP_NT_Taller_Meacanico.Models.Entities
     {
         // esto va a estar comentado hasta que decida si lo genero yo el ID o desde el server
         //private static int genericId = 0;
+        // esto ID probablemente no tenga que estar
+        private int id;
+        private int dni;
+        private string nombre;
+        private Direccion direccion;
+
+        public Cliente(int id, string nombre, Direccion dir)
+        {
+        	
+        }
     }
 }
