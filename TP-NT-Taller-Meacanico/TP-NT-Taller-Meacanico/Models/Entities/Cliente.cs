de4478e12f659adc8f9b6a2bb15a0c3828ccf0d0
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2018-11-01 20:49:33 -0300

    creo 1era clase

---
+++ TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Models/Entities/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP_NT_Taller_Meacanico.Models.Entities
+{
+    public class Cliente
+    {
+        // esto va a estar comentado hasta que decida si lo genero yo el ID o desde el server
+        //private static int genericId = 0;
+    }
+}
