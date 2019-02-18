ffc128bbf1f57ba84dfd321e0816e7ef60183b42
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2019-02-17 21:28:02 -0300

    some files

---
+++ TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Controllers/ClientController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP_NT_Taller_Meacanico.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+
+        public ActionResult ClientMenu()
+        {
+            ViewBag.Message = "This is the client menu";
+            var db = new Models.ProyectoORTEntities3();
+
+            var clients = db.Clients.ToList();
+            // esto es para guardar cualquier cambio realizado 
+            //db.SaveChanges();
+
+            var model = new Models.WorkshopIndex
+            {
+                clients = clients
+            };
+
+            return View(model);
+        }
+
+        public ActionResult AddClient()
+        {
+            ViewBag.Message = "This is the form to add a new client";
+
+            return View();
+        }
+    }
+}
---
+++ TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Models/WorkshopIndex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP_NT_Taller_Meacanico.Models
+{
+    public class WorkshopIndex
+    {
+        public string name { get; set; }
+        public List<Client> clients { get; set; }
+    }
+}
