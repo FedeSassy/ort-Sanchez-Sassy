52495f5ce43f5f5f2fb58a7ac5dc91a5ce5cd106
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2019-02-23 21:26:08 -0300

    scripts

---
+++ TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Scripts/CustomScripts/AjaxCall.js
@@ -0,0 +1,20 @@
+﻿class AjaxClient {
+    static makeAjaxRequest(type, actionName, controllerName, data) {
+        var url = makeUrl(actionName, controllerName);
+        console.log('url => ', url);
+
+        $.ajax({
+            type: type,
+            url: url,
+            contentType: "application/json; charset=utf-8",
+            data: data,
+            dataType: "json",
+            success: function () { alert('Success'); },
+            error: function () { alert('An error'); }
+        });
+    };
+}
+
+function makeUrl(actionName, controllerName) {
+    return '/' + controllerName + '/' + actionName;
+};
---
+++ TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Scripts/CustomScripts/clientManager.js
@@ -0,0 +1,15 @@
+﻿var c_remove = $("#clientRemove");
+
+c_remove.click(function () {
+    removeClientByPersonalID(c_remove.parent().parent()[0].cells[1].textContent);
+});
+
+function removeClientByPersonalID(clientID) {
+    var info = {
+        id: clientID
+    }
+
+    console.log("info => ", info);
+    //static makeAjaxRequest(type, action, controller, data) {
+    AjaxClient.makeAjaxRequest("DELETE", "RemoveClient", "Client", info);
+};
