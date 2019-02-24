6271f3b6f03b06f0d6d277ffc0d5e62c15e3f7d7
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2019-02-24 05:29:27 -0300

    fix reset

--- TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Models/DBWrapper.cs
+++ TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Models/DBWrapper.cs
@@ -6,7 +6,7 @@ namespace TP_NT_Taller_Meacanico.Models
 {
     public class DBWrapper
     {
-        private ProyectoORTEntities3 dbObject;
+        private ProyectoORTEntities5 dbObject;
         private static int ERROR_SAVING = 0;
 
         public DBWrapper()
@@ -32,8 +32,6 @@ namespace TP_NT_Taller_Meacanico.Models
         public void AddClient(Client client)
         {
             client.workshop_id = 1;
-            //TODO no harcodear el address_id
-            client.address_id = 4;
 
             dbObject.Clients.Add(client);
             dbObject.SaveChanges();
@@ -42,26 +40,7 @@ namespace TP_NT_Taller_Meacanico.Models
         public int RemoveClientByPersonalID(int personalID)
         {
             Client client = GetClientByPersonalID(personalID);
-            Address clientAddress = GetAddressByClientID(client.id);
-            if (RemoveAddressFirst(clientAddress) > 1)
-            {
-                dbObject.Clients.Remove(client);
-                return dbObject.SaveChanges();
-            } else
-            {
-                return ERROR_SAVING;
-            }
-
-        }
-
-        private Address GetAddressByClientID(int clientID)
-        {
-            return dbObject.Addresses.Where(a => a.client_id == clientID).ToList().First();
-        }
-
-        private int RemoveAddressFirst(Address address)
-        {
-            dbObject.Addresses.Remove(address);
+            dbObject.Clients.Remove(client);
             return dbObject.SaveChanges();
         }
     }
--- TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Models/ModelDatabase.Designer.cs
+++ TP-NT-Taller-Meacanico/TP-NT-Taller-Meacanico/Models/ModelDatabase.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\nico\Documents\nt-repo\ort-Sanchez-Sassy\TP-NT-Taller-Meacanico\TP-NT-Taller-Meacanico\Models\WorkshopEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\nico\Documents\nt-repo\ort-Sanchez-Sassy\TP-NT-Taller-Meacanico\TP-NT-Taller-Meacanico\Models\ModelDatabase.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
